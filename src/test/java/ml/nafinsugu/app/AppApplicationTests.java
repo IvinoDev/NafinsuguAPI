e60a7f2dcc45a9992dda15d9f0895d336cd0ee16
IvinoDev <kayantao@protonmail.com>
2023-02-24 14:32:44 +0000

    les fichiers de l'api

---
+++ src/test/java/ml/nafinsugu/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.nafinsugu.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
